5ccfe0cda1dffe88d3761271720103ac44ba3b1d
Adithya <adithyamana5517@gmail.com>
2025-09-18 23:44:27 +0530

    vite.config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/', // Must be '/' for Vercel
   plugins: [react()],
-  server: {
-    host: '127.0.0.1', // This forces Vite to use the specific IP address
-    port: 5173,
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
