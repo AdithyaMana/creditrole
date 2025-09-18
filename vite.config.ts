545a07335ef0fcd0e3563c70bdc3be5b5c459eb9
Adithya <adithyamana5517@gmail.com>
2025-09-18 23:11:41 +0530

    Vite Config Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', 
+  base: '/',
   plugins: [react()],
+  server: {
+    host: '127.0.0.1', // This forces Vite to use the specific IP address
+    port: 5173,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
